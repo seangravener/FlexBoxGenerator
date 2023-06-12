8e0466fb87a6530ed15932ced361d17c54b4b816
Sean Gravener <sean@gravener.net>
2023-06-11 21:43:10 -0400

    Refactor model with destructed args

--- src/app/flex-box-generator/components/flex-items/flex-item.model.ts
+++ src/app/flex-box-generator/components/flex-items/flex-item.model.ts
@@ -1,14 +1,16 @@
 import { DEFAULT_CONTENT, DEFAULT_STYLES } from './flex-item.constants';
 import { FlexItemProps, FlexItemStyles } from './flex-item.interface';
 
+const defaults: FlexItemProps = {
+  content: DEFAULT_CONTENT,
+  style: DEFAULT_STYLES,
+};
+
 export class FlexItem implements FlexItemProps {
   content: string;
   style: FlexItemStyles;
 
-  constructor(
-    content: string = DEFAULT_CONTENT,
-    style: FlexItemStyles = DEFAULT_STYLES
-  ) {
+  constructor({ content, style }: FlexItemProps = defaults) {
     this.content = content;
     this.style = style;
   }

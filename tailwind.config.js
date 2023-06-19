5ee636cfa3c8ceb3bf69f043f2a47349daa01c4a
Sean Gravener <sean@gravener.net>
2023-06-19 18:05:51 -0400

    Add a theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
   },
-  plugins: [require('@tailwindcss/typography'), require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };

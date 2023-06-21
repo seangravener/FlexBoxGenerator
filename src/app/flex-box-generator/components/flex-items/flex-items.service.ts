80b3a9ba79e6fe86778a8421779dd101b1f44f14
Sean Gravener <sean@gravener.net>
2023-06-20 21:44:23 -0400

    Add a dash of emoji 🧑‍🍳

--- src/app/flex-box-generator/components/flex-items/flex-items.service.ts
+++ src/app/flex-box-generator/components/flex-items/flex-items.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { StateService } from '../../core/services/state.service';
-import { filter, map, share, shareReplay, startWith, takeWhile, tap } from 'rxjs';
+import { map, shareReplay, takeWhile, tap } from 'rxjs';
 import { FlexItem } from './flex-item.model';
-import { DEFAULT_FLEX_ITEMS } from './flex-item.constants';
+import { DEFAULT_FLEX_ITEMS, DEFAULT_STYLES } from './flex-item.constants';
 
 @Injectable({ providedIn: 'root' })
 export class FlexItemsService {
@@ -12,13 +12,16 @@ export class FlexItemsService {
 
   flexItems$ = this.stateService.get<FlexItem[]>('flexItems').pipe(
     takeWhile((items) => Boolean(items)),
-    map((items) => items.map((item) => new FlexItem(item))),
+    map((items) => items.map((item) => new FlexItem({ ...item }))),
     tap((items) => (this.flexItems = items)),
     shareReplay()
   );
 
   addItem() {
-    this.stateService.set('flexItems', [...this.flexItems, new FlexItem()]);
+    this.stateService.set('flexItems', [
+      ...this.flexItems,
+      new FlexItem({ style: DEFAULT_STYLES, content: this.generateEmoji() }),
+    ]);
   }
 
   deleteItem() {
@@ -27,5 +30,11 @@ export class FlexItemsService {
 
   resetItems() {
     this.stateService.set('flexItems', DEFAULT_FLEX_ITEMS);
+  }
+
+  generateEmoji() {
+    // prettier-ignore
+    const possibleEmojis = ['👋','😋','💯','🎱','🎨','👍','💡','🫧','⚡','👊'];
+    return possibleEmojis[Math.floor(Math.random() * possibleEmojis.length)];
   }
 }

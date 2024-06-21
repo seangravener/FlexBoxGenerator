51c4f69159d571585f2147445bf2ee6bceafd5bc
Sean Gravener <sean@gravener.net>
2024-06-21 19:54:42 -0400

    refactor: Update default flex items in flex-items.ts

--- src/app/flex-box-generator/core/fixtures/flex-items.ts
+++ src/app/flex-box-generator/core/fixtures/flex-items.ts
@@ -1,8 +1,8 @@
-import { FLEX_ITEM } from '../../components/flex-items/flex-item.constants'
+import { DEFAULT_FLEX_ITEM } from '../../components/flex-items/flex-item.constants'
 import { FlexItem } from '../../components/flex-items/flex-item.model'
 import { Slice } from '../services/store.provider'
 
-export const FLEX_ITEMS = [FLEX_ITEM, FLEX_ITEM, FLEX_ITEM] as FlexItem[]
+export const FLEX_ITEMS = [DEFAULT_FLEX_ITEM, DEFAULT_FLEX_ITEM, DEFAULT_FLEX_ITEM] as FlexItem[]
 export const FLEX_ITEMS_SLICE: Slice<FlexItem[]> = {
   flexItems: FLEX_ITEMS,
 } as Slice<FlexItem[]>

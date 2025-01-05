e8a88bb9888139d67c2a4614af1de3bd72bb51c8
Sean Gravener <sean@gravener.net>
2025-01-05 08:19:59 -0500

    Fix: Output styles in kebab-case (not camelCase)

--- src/app/flex-box-generator/components/flex-container/flex-container.component.ts
+++ src/app/flex-box-generator/components/flex-container/flex-container.component.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core'
 import { FlexItemsService } from '../flex-items/flex-items.service'
 import { FlexContainerService } from './flex-container.service'
-import { Observable } from 'rxjs'
+import { map, Observable } from 'rxjs'
 import { FlexContainer } from './flex-container.model'
 
 @Component({
@@ -21,7 +21,12 @@ export class FlexContainerComponent {
 
   flexItems$ = this.flexItemsService.flexItems$
   flexContainer$: Observable<FlexContainer> =
-    this.flexContainerService.flexContainer$
+    this.flexContainerService.flexContainer$.pipe(
+      map((flexContainer) => {
+        flexContainer.style = this.convertObjectKeysToKebab(flexContainer.style)
+        return flexContainer
+      }),
+    )
 
   constructor(
     private readonly flexItemsService: FlexItemsService,
@@ -29,7 +34,21 @@ export class FlexContainerComponent {
   ) {}
 
   onContainerChange(flexItems: any[]) {
-    // this.flexContainerService.set<FlexContainerProps>(flexItems);
     this.containerChanges.emit(flexItems)
+  }
+
+  private camelToKebab(camelCaseString: string) {
+    return camelCaseString.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+  }
+
+  private convertObjectKeysToKebab(obj: any) {
+    const result: { [key: string]: string } = {}
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const kebabKey = this.camelToKebab(key)
+        result[kebabKey] = obj[key]
+      }
+    }
+    return result
   }
 }
